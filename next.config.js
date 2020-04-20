68a0ced783e5ef140958a4116c4a09283633402c
Aristos Markogiannakis <aristosmarkogiannakis@aristoss-mbp.home>
2020-04-20 23:54:21 +0100

    added extra

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withSass({
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      "/": { page: "/", query: { missings: 'toiletroll' } },
+      "/": { page: "/", query: { missing: 'toiletroll' } },
       "/api/addstores": {page: "/api/addstores"},
       "/p/hello-nextjs": {page: "/api/stores", query: { missings: 'toiletroll' }},
       "/p/hello-nextjs": {page: "/api/stores", query: { missings: 'bread' }},
