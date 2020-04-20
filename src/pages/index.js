a3b735242bcce8f6532685ea3de7586f3c43c2d4
Aristos Markogiannakis <aristosmarkogiannakis@aristoss-mbp.home>
2020-04-21 00:27:03 +0100

    added extra

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ const Index = function ({stores, missing})  {
 
 Index.getInitialProps = async ({req}) => {
 
-  const {missing} = req.query;
+  const {missing} = req.hasOwnProperty('query')?req.query:'';
 
   const tes = await fetch(`${process.env.SERVER}/api/stores?missing=${missing}`);
   const data = await tes.json();
