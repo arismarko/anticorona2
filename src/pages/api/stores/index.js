884e19c5e287c57916c0c5d3a434cb017ab94902
mstam <m.stamoulos@gmail.com>
2020-04-26 16:20:47 +0100

    addind total

--- src/pages/api/stores/index.js
+++ src/pages/api/stores/index.js
@@ -9,26 +9,52 @@ const prisma = new PrismaClient();
 // Required fields in body: name, email
 export default async (req, res) => {
     const {
-        query: {missing },
-        query: {date},
+        query: { missing },
+        query: { date },
         method,
     } = req
 
     switch (method) {
         case 'GET':
             // Get data from your database
-               await prisma.store.findMany({ where: 
-                    { 
-                        date: new Date(date),
-                        Item: {
-                            some: 
-                                {item: missing} 
-                        } 
+            await prisma.store.findMany({
+                include: {
+                    StoresOnItems: true,
+                    Item: true
+                },
+                where:
+                {
+                    date: new Date(date),
+                    Item: {
+                        some:
+                            { item: missing }
                     }
-                }).then(results => {
-                    console.log(results);
-                    res.status(200).json(results)
-                })
+                }
+            }).then(results => {
+                console.log('here', results);
+                results.forEach(function (value) {
+                    //  console.log('va;', value.Item);
+
+                    var total = value.Item.reduce(function (previousValue, currentValue) {
+
+
+
+                        return {
+                            id: undefined,
+                            item: currentValue.item + ' total',
+                            number: previousValue === undefined ? 0 : parseInt(previousValue.number) + parseInt(currentValue.number)
+
+                        }
+                    });
+
+                    value.total = total;
+                });
+
+
+
+
+                res.status(200).json({ results });
+            })
 
             break
         case 'POST':
@@ -36,22 +62,24 @@ export default async (req, res) => {
             const { body } = req;
 
             var today = new Date();
-            var todayonlydate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var todayonlydate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
-            const store =  await prisma.store.create({ data: {
-                storename: body.storename,
-                location: body.location,
-                coordinates: body.coordinates,
-                date: new Date(todayonlydate),
-                Item: {
-                create: body.items,
-                },
-            }});
-    
+            const store = await prisma.store.create({
+                data: {
+                    storename: body.storename,
+                    location: body.location,
+                    coordinates: body.coordinates,
+                    date: new Date(todayonlydate),
+                    Item: {
+                        create: body.items,
+                    },
+                }
+            });
+
             res.status(200).json(store);
             break
         default:
             res.setHeader('Allow', ['GET', 'PUT'])
             res.status(405).end(`Method ${method} Not Allowed`)
-      }
+    }
 }
