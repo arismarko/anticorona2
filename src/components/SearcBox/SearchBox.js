93537c6d33b9a67cb236ed7c063b38c6c18dac4c
Aris(WiproDigital) <ariswiprodigital@ariswipitalsmbp.home>
2020-04-26 11:58:02 +0100

    added

--- src/components/SearcBox/SearchBox.js
+++ src/components/SearcBox/SearchBox.js
@@ -16,7 +16,7 @@ const SearchBox = () => {
                     }}
                     onSubmit={(values, { setSubmitting }) => {
                         setTimeout(() => {
-                            router.push(`/?missing=${values.search.trim().toLowerCase()}`)
+                            router.push(`/?missing=${values.search.toLowerCase()}`)
                         }, 400);
                     }}
             >
